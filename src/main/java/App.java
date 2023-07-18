3045b90c266df26a33a16ee81fd7cf05f899705c
Mark <graf.crab@gmail.com>
2023-07-18 16:46:45 +0300

    start

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
+
+    //16.45 start
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
